12b56cc3da4421356a32618bdac4e8926dbc18d3
IsraelPossidonioSilva <devmobilejr@gmail.com>
2024-05-22 17:10:52 -0300

    Criando a classe PosicaoXadrez

---
+++ XadrezConsole/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,22 @@
+﻿using tabuleiro;
+namespace xadrez {
+    class PosicaoXadrez {
+           
+        public char coluna {  get; set; }
+        public int linha {  get; set; }
+
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        //converter a posição do xadrez para uma posição interna de matriz
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+    }
+}
